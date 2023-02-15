de7e8c800ce14f1e6c4ab427eada2e15199b2610
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-15 16:57:37 -0500

    Created rw functions for table, more templating

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -1,33 +1,35 @@
 /* TODO: Create Table Main Page */
-import { ReadCollection, WriteDocument, DeleteDocument } from "@/lib/firebase/FirestoreOperations";
+import { ReadTable, ReadTables, WriteTable, DeleteTable } from "@/lib/firebase/table/TableOperations";
+import { Timestamp } from "firebase/firestore";
 
-import { QuerySnapshot, QueryDocumentSnapshot, DocumentData} from "firebase/firestore"
-import Table from "../../types/Table";
+import { Table } from "../../types/Table";
 import { useState } from "react";
 
 export default function TablePage() {
-  const [content, setContent] = useState<any>();
+  const [content, setContent] = useState<{[id: string]: Table}>();
   const [doc, setDoc] = useState("");
 
   const col = "tables";
 
   const data: Table = {
-    generated: (new Date()).toISOString(),
+    name: "",
+    lastAccessed: Timestamp.fromDate(new Date()),
     users: [],
-    leader: ""
+    leader: "",
+    expiration: Timestamp.fromDate(new Date((new Date()).getTime() + 60 * 60 * 24 * 1000)),
   }
 
   const writeData = async () => {
-    await WriteDocument(col, data)
+    await WriteTable(data)
   }
 
   const readData = async () => {
-    const dataAccessed = await ReadCollection(col)
-    setContent(dataAccessed?.docs)
+    const dataAccessed = await ReadTables()
+    setContent(dataAccessed)
   }
 
   const deleteData = async () => {
-    await DeleteDocument(col, doc)
+    await DeleteTable(doc)
     await readData()
     setDoc("")
   }
@@ -39,12 +41,12 @@ export default function TablePage() {
       <button onClick={ deleteData }>delete {doc}</button>
       <input onChange={ (e) => {setDoc(e.currentTarget.value)} } value={doc}></input>
       <ul>
-      { content?.map( 
-        (doc: any) => {
+      { content ? Object.keys(content).map( 
+        (key) => {
           return (
-            <li key={doc.id}>{doc.id + "=>" + (doc.data() as Table).generated}</li>
+            <li key={key}>{key + ": " + content[key].lastAccessed.toDate() + "=>" + content[key].expiration.toDate()}</li>
           )
-        })
+        }) : null
       }
       </ul>
     </>
