3850ecfda49526077b532ed7b40f0bb4413579b9
prabhav-pande <prabhav2833@gmail.com>
2023-02-16 18:17:45 -0500

    Update resetpass.tsx
    Using form component and async function.

--- src/pages/auth/resetpass.tsx
+++ src/pages/auth/resetpass.tsx
@@ -1,16 +1,35 @@
 /* TODO: Make reset password page */
 import { useState } from "react";
 
-const reset = () => {
 
-}
 export default function resetPass() {
     const [email, setEmail] = useState("");
+
+    const handleReset = async (e : any) => {
+        console.log("helloworld");
+        e.preventDefault();
+    };
+
     return (
+        /* html type
         <>
             <>Enter your email: </>
             <input onChange={ (e) => {setEmail(e.currentTarget.value)} } value={email}></input>
             <button onClick={ reset }>Reset Password</button>      
         </>
+        */
+       //NextJS form component
+       <div>
+       <form onSubmit={(e) => handleReset(e)}>
+         <input
+           type="email"
+           placeholder = "Please enter your email"
+           value={email}
+           onChange={(e) => setEmail(e.target.value)}
+         />
+ 
+         <button type="submit">Submit</button>
+       </form>
+        </div>
     );
 }
