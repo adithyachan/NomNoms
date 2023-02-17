dae17661df452ac8e2be0d1afa6a6ac009ff117f
rjagana <rjagana@purdue.edu>
2023-02-17 14:11:58 -0500

    Working create account page w/ email and password input

---
+++ src/pages/auth/createAccount.tsx
@@ -0,0 +1,51 @@
+/* TODO: Make Create Account Page */
+import { useFirebaseApp, useFirebaseAuth } from "@/lib/firebase/hooks/useFirebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+
+
+
+export default function CreateAccount() {
+  //create-account
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+
+  const handleCreate = async (e : any) => {
+      console.log("working");
+      e.preventDefault();
+      const auth = useFirebaseAuth();
+  };
+
+  return (
+      
+     //NextJS form component
+     <div>
+     <form onSubmit={(e) => handleCreate(e)}>
+       <input
+         type="email"
+         placeholder = "Email Address"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+       />
+       <text>{"\n"}</text>
+       <input
+         type="password"
+         placeholder = "Password"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+       />
+       <text>{"\n"}</text>
+       <input
+         type="confirmpassword"
+         placeholder = "Confirm Password"
+         value={confirmpassword}
+         onChange={(e) => setConfirmPassword(e.target.value)}
+       />
+       <text>{"\n"}</text>
+       <button type="submit">Submit</button>
+     </form>
+      </div>
+  );
+}
+ 
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,9 @@
 /* TODO: Make Login Page */
+import { useFirebaseApp, useFirebaseAuth } from "@/lib/firebase/hooks/useFirebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+
+
 
 export default function Login() {
   return (
