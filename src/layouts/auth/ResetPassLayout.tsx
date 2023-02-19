2f0b23ea041ccc413552bc032194c9a2b7b11a4b
prabhav-pande <prabhav2833@gmail.com>
2023-02-19 17:27:58 -0500

    Update ResetPassLayout.tsx

--- src/layouts/auth/ResetPassLayout.tsx
+++ src/layouts/auth/ResetPassLayout.tsx
@@ -32,13 +32,11 @@ import { useForm } from 'react-hook-form'
       fontWeight: 900,
       fontFamily: `Greycliff CF, ${theme.fontFamily}`,
     },
-  
     controls: {
       [theme.fn.smallerThan('xs')]: {
         flexDirection: 'column-reverse',
       },
     },
-  
     control: {
       [theme.fn.smallerThan('xs')]: {
         width: '100%',
@@ -60,14 +58,12 @@ import { useForm } from 'react-hook-form'
     const router = useRouter();
     const [isError, setError] = useState(Boolean);
 
-    //const handleReset = async (e : any) => {}
     const handleReset = async (e : any) => {
       console.log(getValues('email'));
       e.preventDefault();
       const email = getValues('email');   
       const auth = useFirebaseAuth();
-      //fix edge cases  
-      sendPasswordResetEmail(auth, email).then(() => {
+        sendPasswordResetEmail(auth, email).then(() => {
         setError(false);
         router.push('/auth/inputresetpass')
       }).catch(error => {
@@ -84,40 +80,46 @@ import { useForm } from 'react-hook-form'
 
     return (
       <form onSubmit={handleReset}>
-    <Container size={460} my={30}>
-        <Image width={400} src="/images/full_logo.png" alt="Main NomNoms Logo" className="self-center"/>
-        <Title className={classes.title} align="center">
-          Forgot your password?
-        </Title>
-        <Text color="dimmed" size="sm" align="center">
-          Enter your email to get a reset link
-        </Text>
-        <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+        <Container size={460} my={30} 
+          className="mt-40 bg-gradient-to-r from-rose-50 via-white to-rose-50 p-10 rounded-xl shadow-rose-200 shadow-lg transition ease-in-out duration-300 hover:shadow-2xl hover:shadow-rose-300">
+          <Center className="flex-col">
+            <Image width={400} src="/images/full_logo.png" alt="Main NomNoms Logo" className="self-center"/>
+            <Title className={classes.title} align="center">
+              Forgot your password?
+            </Title>
+            <Text color="dimmed" size="sm" align="center">
+              Enter your email to get a reset link
+            </Text>
+            <Paper withBorder shadow="md" p={30} radius="md" mt="xl" className='bg-rose-200'>
 
-        <TextInput  
-        label="Your email" 
-        placeholder="nomnoms@gmail.com" 
-        classNames={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? { input: classes.invalid } : {}}
-        rightSection={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? <IconAlertTriangle stroke={1.5} size={16} className={classes.icon} /> : <></>}
-        error={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? "Invalid email" : ""}  
-        {...register("email", 
-        { required: true, 
-        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i 
-        })}
-        />
-          <Group position="apart" mt="lg" className={classes.controls}>
-            <Anchor color="dimmed" size="sm" className={classes.control}>
-              <Center inline>
-                <IconArrowLeft size={12} stroke={1.5} />
-                <Box onClick={handleReturn} ml={5}>Back to login page</Box>
-              </Center>
-            </Anchor>
-            <Button className={classes.control} disabled={!isValid} type="submit" >Reset password</Button>
-          </Group>
-        </Paper>
-
-      </Container>
-
+            <TextInput  
+            label="Your email" 
+            placeholder="nomnoms@gmail.com" 
+            classNames={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? { input: classes.invalid } : {}}
+            rightSection={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? <IconAlertTriangle stroke={1.5} size={16} className={classes.icon} /> : <></>}
+            error={errors?.email?.type == 'pattern' || errors?.email?.type == 'required' ? "Invalid email" : ""}  
+            {...register("email", 
+            { required: true, 
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i 
+            })}
+            />
+              <Group position="apart" mt="lg" className={classes.controls}>
+                <Anchor color="red" size="sm" className={classes.control}>
+                  <Center inline>
+                    <IconArrowLeft color='#F43F5E' size={12} stroke={1.5} />
+                    <Box onClick={handleReturn} ml={5}>
+                      <Text color='#F43F5E'>
+                        Back to login page
+                      </Text>
+                      
+                    </Box>
+                  </Center>
+                </Anchor>
+                <Button className={`bg-rose-500 hover:bg-rose-600 ${classes.control}`} disabled={!isValid} type="submit" >Reset password</Button>
+              </Group>
+            </Paper>
+          </Center>
+        </Container>
       </form>
     );
   }
