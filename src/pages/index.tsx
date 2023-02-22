06603b3c7c12eb01d5761e67613dc100595207b4
Jay Mehta <jaysmehta@gmail.com>
2023-02-22 17:39:37 -0500

    started cards component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { useFirebaseApp } from '../lib/firebase/hooks/useFirebase'
 import JoinTable from '@/components/table/JoinTable'
 import {GetRestaurants, GetRestaurant} from '@/lib/utils/yelpAPI'
+import RenderImage from '@/components/table/restaurantCards/Image'
 export default function Home() {
   // start up firebase
   const app = useFirebaseApp()
@@ -23,6 +24,7 @@ export default function Home() {
 =======
         <p>NomNoms</p>
         <JoinTable />
+        <RenderImage url ="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg" name = "Scenery"/>
         {/* <GetRestaurants zip="10019" radius="8000" categories="japanese"/> */}
         <GetRestaurant id="TN4RnyqHMSupRFot4Q-_EA"/>
 >>>>>>> ef9675a (finished testing api call)
