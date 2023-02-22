51b35346a3a58ff02827c51e59ea16f3a275045e
Jay Mehta <jaysmehta@gmail.com>
2023-02-22 15:09:00 -0500

    finished testing api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import LandingLayout from '@/layouts/LandingLayout'
 import Head from 'next/head'
 import { useFirebaseApp } from '../lib/firebase/hooks/useFirebase'
-import JoinTable from '@/components/table/tableSelection/joinTable/JoinTable'
-import GetRestaurants from '@/lib/utils/yelpAPI'
+import JoinTable from '@/components/table/JoinTable'
+import {GetRestaurants, GetRestaurant} from '@/lib/utils/yelpAPI'
 export default function Home() {
   // start up firebase
   const app = useFirebaseApp()
@@ -17,8 +17,15 @@ export default function Home() {
       </Head>
       <main>
         {/* TODO: Create Basic Landing */}
+<<<<<<< HEAD
 
         <LandingLayout />
+=======
+        <p>NomNoms</p>
+        <JoinTable />
+        {/* <GetRestaurants zip="10019" radius="8000" categories="japanese"/> */}
+        <GetRestaurant id="TN4RnyqHMSupRFot4Q-_EA"/>
+>>>>>>> ef9675a (finished testing api call)
       </main>
     </>
   )
