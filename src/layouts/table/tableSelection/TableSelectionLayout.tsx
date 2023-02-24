94bc44db85102b986ece80249399303481f04bb2
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-24 14:02:31 -0500

    User tables cards and responsive formatting

--- src/layouts/table/tableSelection/TableSelectionLayout.tsx
+++ src/layouts/table/tableSelection/TableSelectionLayout.tsx
@@ -6,11 +6,11 @@ export default function TableSelectionLayout() {
   return (
     <Container fluid className="bg-gradient-to-b from-rose-100 to-white">
       <Grid>
-        <Grid.Col span={6}>
-          <UserTablesLayout />
-        </Grid.Col>
-        <Grid.Col span={6}>
+        <Grid.Col span={12} md={6}>
           <CreateJoinTableLayout />
+        </Grid.Col>
+        <Grid.Col span={12} md={6}>
+          <UserTablesLayout />
         </Grid.Col>
       </Grid>
     </Container>
--- src/layouts/table/tableSelection/UserTablesLayout.tsx
+++ src/layouts/table/tableSelection/UserTablesLayout.tsx
@@ -1,8 +1,34 @@
+import TableCard from "@/components/table/tableSelection/TableCard";
+import { Container, Grid, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { ReadTables } from "@/lib/firebase/table/TableOperations";
+import { Table } from "@/types/Table";
+
 export default function UserTablesLayout() {
+  const [tables, setTables] = useState<{[id: string]: Table}>({})
+
+  const getTables = async () => {
+    setTables(await ReadTables())
+  }
+
+  useEffect(() => {
+    getTables()
+  }, [])
+
   return(
     <>
-    {/* TODO: Populate with cards of each of user's tables */}
-      MyTablesLayout
+      <Container className="bg-white p-10 mt-10 rounded-3xl shadow-lg shadow-rose-100">
+        <Text className="mb-10 text-center text-5xl font-black" variant="gradient" gradient={{from: "red.7", to: "red.4"}}>Your Tables</Text>
+        <Grid>
+          {Object.keys(tables).map(
+            (id) =>
+            <Grid.Col key={id} span={12} sm={6}>
+              <TableCard table={tables[id]} id={id} />
+            </Grid.Col>
+          )}
+        </Grid>
+      </Container>
+      
     </>
   );
 }
