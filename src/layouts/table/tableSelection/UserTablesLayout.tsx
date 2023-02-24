e8b526cc74602f906e52ea26ab571548e9af56bd
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-24 17:14:25 -0500

    Added scroll to cards

--- src/layouts/table/tableSelection/UserTablesLayout.tsx
+++ src/layouts/table/tableSelection/UserTablesLayout.tsx
@@ -1,33 +1,44 @@
 import TableCard from "@/components/table/tableSelection/TableCard";
-import { Container, Grid, Text } from "@mantine/core";
+import { Center, Flex, Grid, ScrollArea, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { ReadTables } from "@/lib/firebase/table/TableOperations";
 import { Table } from "@/types/Table";
+import LoadingLayout from "@/layouts/LoadingLayout";
 
 export default function UserTablesLayout() {
   const [tables, setTables] = useState<{[id: string]: Table}>({})
+  const [loading, setLoading] = useState(false)
 
   const getTables = async () => {
     setTables(await ReadTables())
   }
 
   useEffect(() => {
+    setLoading(true)
     getTables()
+    setLoading(false)
   }, [])
 
   return(
     <>
-      <Container className="bg-white p-10 mt-10 rounded-3xl shadow-lg shadow-rose-100">
-        <Text className="mb-10 text-center text-5xl font-black" variant="gradient" gradient={{from: "red.7", to: "red.4"}}>Your Tables</Text>
-        <Grid gutter={50}>
-          {Object.keys(tables).map(
-            (id) =>
-            <Grid.Col key={id} span={12} sm={6}>
-              <TableCard table={tables[id]} id={id} />
-            </Grid.Col>
-          )}
-        </Grid>
-      </Container>
+      <Flex className="bg-white p-10 mt-10 rounded-3xl shadow-lg shadow-rose-100 flex-col justify-center">
+        <Text className="mb-10 text-5xl text-center font-black" variant="gradient" gradient={{from: "red.7", to: "red.4"}}>Your Tables</Text>
+          { loading ?
+           <LoadingLayout /> 
+           : 
+           <ScrollArea type="hover" className="h-60" scrollbarSize={0}>
+            <Center>
+              <Grid columns={24} className="m-1">
+                {Object.keys(tables).map(
+                  (id) =>
+                  <Grid.Col key={id} span={22} sm={12}>
+                    <TableCard table={tables[id]} id={id} />
+                  </Grid.Col>
+                )}
+              </Grid>
+            </Center>
+          </ScrollArea> }
+      </Flex>
       
     </>
   );
