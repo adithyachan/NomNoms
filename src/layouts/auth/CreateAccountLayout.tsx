459c98728f9163ad7cdb5c3108dd8de02d17bd92
rjagana <rjagana@purdue.edu>
2023-02-25 15:30:29 -0500

    updated create account ui

--- src/layouts/auth/CreateAccountLayout.tsx
+++ src/layouts/auth/CreateAccountLayout.tsx
@@ -19,7 +19,6 @@ import {
   Divider,
   useMantineColorScheme,
   ColorSchemeProvider,
-  NavLink,
   Checkbox,
   Anchor,
   Stack,
@@ -42,13 +41,13 @@ export default function AuthenticationForm(props: PaperProps) {
     validate: {
       email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
       password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
-      confirmpassword: (val, values) =>
-        val !== values.password ? 'Passwords did not match' : null,
-  },
+      confirmpassword: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
   });
 
   //disable the button if the inputs are not valid
     const handleCreate = async (e : any) => {
+      console.log("hello");
       const auth = useFirebaseAuth();
       createUserWithEmailAndPassword(auth, form.values.email, form.values.password)
       .then((userCredential) => {
@@ -105,10 +104,21 @@ export default function AuthenticationForm(props: PaperProps) {
         
 
       <form onSubmit={handleCreate}>
-      <Container size={460} my={30} 
+      <Container size={500} my={50} 
           className="mt-40 bg-gradient-to-r from-rose-50 via-white to-rose-50 p-10 rounded-xl shadow-rose-200 shadow-lg transition ease-in-out duration-300 hover:shadow-2xl hover:shadow-rose-300">
-        
-    
+          <Image width={400} src="/images/full_logo.png" alt="Main NomNoms Logo" className="self-center"/>
+          {type === 'register' && (
+
+          <Title className= {classes.title} align = "center">
+              Not a Nomster yet?
+          </Title>
+          )}
+
+          {type === 'register' && (
+          <Text color="dimmed" size="sm" align="center">
+              Create an account below!
+            </Text>
+          )}
 
         <Stack>
 
@@ -137,7 +147,7 @@ export default function AuthenticationForm(props: PaperProps) {
                 placeholder="Confirm your password"
                 value={form.values.confirmpassword}
                 onChange={(event) => form.setFieldValue('confirmpassword', event.currentTarget.value)}
-                error={form.errors.confirmpassword && 'Passwords did not match'}
+                error={form.errors.confirmpassword && 'Password should include at least 6 characters'}
             />
           )}
 
@@ -148,7 +158,7 @@ export default function AuthenticationForm(props: PaperProps) {
                 
             <Checkbox
               label="I accept terms and conditions"
-              checked={form.values.terms}
+              checked={false}
               color = "pink"
               onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
             />
