638006e798524b37bef764b18f02e8fb36b25697
Jay Mehta <jaysmehta@gmail.com>
2023-02-26 16:31:33 -0500

    fixing conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import LandingLayout from '@/layouts/LandingLayout'
 import Head from 'next/head'
+import { useState } from 'react'
 import { useFirebaseApp } from '../lib/firebase/hooks/useFirebase'
-import JoinTable from '@/components/table/JoinTable'
-import {GetRestaurants, GetRestaurant} from '@/lib/utils/yelpAPI'
+import RenderImage from '@/components/table/restaurantCards/Image'
+
+import {useRestaurantBusinessEndpoint, useRestaurantListEndpoint} from '@/lib/utils/yelpAPI'
+import ShowCard from '@/components/table/restaurantCards/Card'
 export default function Home() {
   // start up firebase
   const app = useFirebaseApp()
+  const {data: listData, error: listError, isLoading: isLoadingList} = useRestaurantListEndpoint(10019, 3200, "japanese%2Cchinese")
+  const {data: businessData, error: businessError, isLoading: isLoadingBusiness} = useRestaurantBusinessEndpoint("TN4RnyqHMSupRFot4Q-_EA")
 
+  function TestListHook() {
+    
+    if (listData) return <>List loaded successfully</>
+    if (listError) return <>Error loading list</>
+    else return <>Loading list...</>
+    
+  }
+
+  function TestBusinessHook() {
+    if (businessData) return <>Business loaded successfully</>
+    if (businessError) return <>Error loading business</>
+    else return <>Loading business...</>
+  }
   return (
     <>
       <Head>
@@ -17,8 +35,8 @@ export default function Home() {
       </Head>
       <main>
         {/* TODO: Create Basic Landing */}
-
-        <LandingLayout />
+        <TestBusinessHook />
+        <TestListHook />
       </main>
     </>
   )
