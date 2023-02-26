b161e83c0dc32bff82c9d7baeb427040a08ce456
albearx <albert.xu.13103@icloud.com>
2023-02-25 19:28:58 -0500

    Fixed custom API hooks
    Fixed the issue where hooks would not return the correct status dynamically and could not be used in parallel.

--- src/lib/utils/yelpAPI.tsx
+++ src/lib/utils/yelpAPI.tsx
@@ -10,19 +10,9 @@ const options = {
 const fetcher = (url: string) => fetch(url, options).then(res => res.json());
 
 export function useRestaurantListEndpoint(zip: number, radius: number, categories: string) {
-  const {data, error, isLoading} = useSWR(`/api/businesses?limit=50&location=${zip}&radius=${radius}&categories=${categories}`, fetcher);
-  if (error) return error
-  if (isLoading) return 'loading'
-  console.log('Restaurant list search:')
-  console.log(data)
-  return data
+  return useSWR(`/api/businesses?limit=50&location=${zip}&radius=${radius}&categories=${categories}`, fetcher);
 }
 
 export function useRestaurantBusinessEndpoint(id: string) {
-  const {data, error, isLoading} = useSWR(`/api/business?id=${id}`, fetcher);
-  if (error) return error
-  if (isLoading) return 'loading'
-  console.log('Restaurant search by ID')
-  console.log(data)
-  return data
+  return useSWR(`/api/business?id=${id}`, fetcher);
 }
