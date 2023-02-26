1cca32bfee0032aa47e74e66395c9cb28ca3f562
albearx <albert.xu.13103@icloud.com>
2023-02-25 19:28:58 -0500

    Fixed custom API hooks
    Fixed the issue where hooks would not return the correct status dynamically and could not be used in parallel.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import { useFirebaseApp } from '../lib/firebase/hooks/useFirebase'
 import JoinTable from '@/components/table/JoinTable'
 import {useRestaurantBusinessEndpoint, useRestaurantListEndpoint} from '@/lib/utils/yelpAPI'
@@ -7,8 +8,22 @@ import RenderImage from '@/components/table/restaurantCards/Image'
 export default function Home() {
   // start up firebase
   const app = useFirebaseApp()
-  const restaurantList = useRestaurantListEndpoint(10019, 3200, "japanese%2Cchinese")
-  const restaurant = useRestaurantBusinessEndpoint("TN4RnyqHMSupRFot4Q-_EA")
+  const {data: listData, error: listError, isLoading: isLoadingList} = useRestaurantListEndpoint(10019, 3200, "japanese%2Cchinese")
+  const {data: businessData, error: businessError, isLoading: isLoadingBusiness} = useRestaurantBusinessEndpoint("TN4RnyqHMSupRFot4Q-_EA")
+
+  function TestListHook() {
+    
+    if (listData) return <>List loaded successfully</>
+    if (listError) return <>Error loading list</>
+    else return <>Loading list...</>
+    
+  }
+
+  function TestBusinessHook() {
+    if (businessData) return <>Business loaded successfully</>
+    if (businessError) return <>Error loading business</>
+    else return <>Loading business...</>
+  }
   return (
     <>
       <Head>
@@ -20,10 +35,10 @@ export default function Home() {
       <main>
         {/* TODO: Create Basic Landing */}
         <p>NomNoms</p>
-        <JoinTable />
-        <RenderImage url ="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg" name = "Scenery"/>
+        <TestListHook /> <br />
+        <TestBusinessHook />
+        {/* <RenderImage url ="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg" name = "Scenery"/> */}
         {/* <GetRestaurants zip="10019" radius="8000" categories="japanese"/> */}
-        <GetRestaurant id="TN4RnyqHMSupRFot4Q-_EA"/>
       </main>
     </>
   )
