5f8028f545981257ca72b3b7e493ebc5eab96f32
albearx <albert.xu.13103@icloud.com>
2023-02-26 16:39:33 -0500

    Create CardStack.tsx
    Began development on the stack of restaurant cards (for user votes on restaurants)

---
+++ src/components/table/restaurantCards/CardStack.tsx
@@ -0,0 +1,25 @@
+import ShowCard from "./Card";
+import { useState } from 'react'
+
+import { Card, Image, Text, Badge, Button, Group, Popover } from '@mantine/core';
+import { useRestaurantBusinessEndpoint } from '@/lib/utils/yelpAPI';
+import { Loader } from '@mantine/core';
+import { BackgroundImage, Center } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export default function CardStack({ids} : any) {
+  if (ids.length == 0) return (<Text size="md" color="dimmed">Card stack is empty!</Text> )
+  let index = 0;
+  const [id, setID] = useState(ids[index])
+
+  return (
+    <>
+      <ShowCard id={id} />
+      <div style={{display: 'flex', gap: 10}}>
+        <Button color="green">Yes</Button>
+        <Button color="red">No</Button>
+      </div>
+    </>
+  )
+  
+}
