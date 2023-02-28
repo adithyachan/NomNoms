c64493daab7fd5c8e31bae59662976e9b8a678ea
rjagana <rjagana@purdue.edu>
2023-02-28 14:24:36 -0500

    added github authentication

--- src/lib/firebase/auth/AuthService.tsx
+++ src/lib/firebase/auth/AuthService.tsx
@@ -1,5 +1,5 @@
 import { useFirebaseAuth } from "@/lib/firebase/hooks/useFirebase"
-import {signInWithPopup, createUserWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider, signOut } from "firebase/auth";
+import {signInWithPopup, createUserWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider, signOut, GithubAuthProvider } from "firebase/auth";
 import { StringLiteral } from "typescript";
 
 
@@ -72,6 +72,31 @@ export async function CreateAccountWithFacebook() {
   });
   console.log("checking facebook")  
   
+}
+
+export async function CreateAccountWithGitHub() {
+	const provider = new GithubAuthProvider();
+	const auth = useFirebaseAuth();
+ signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+  });
 }
 
 export async function SignOutofAccount() {
