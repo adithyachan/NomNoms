63736c07a162702d959397ffa47712a72af44a0b
Jay Mehta <jaysmehta@gmail.com>
2023-02-28 18:51:04 -0500

    for testing

---
+++ src/pages/testing/index.tsx
@@ -0,0 +1,11 @@
+import ShowCard from "@/components/table/restaurantCards/Card";
+export default function Test() {
+    return (
+      <>    
+            <div style={{ display: "flex" }}>
+             <ShowCard id = "qY1o1MhoRyN84RRhUJtvpQ" />
+             <ShowCard id = "xWUL-j6TQyKQHIfOtetu-Q" />
+            </div>
+      </>           
+    );
+}
