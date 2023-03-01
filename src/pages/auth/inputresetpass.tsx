148754f144cdd52bd040c4fffb5f627b61803d3a
prabhav-pande <prabhav2833@gmail.com>
2023-03-01 01:10:59 -0500

    Update to pass checks hopefully

--- src/pages/auth/inputresetpass.tsx
+++ src/pages/auth/inputresetpass.tsx
@@ -1,5 +1,4 @@
 /* TODO: Input Reset Password Page */
-import ProtectedRoute from "@/components/auth/ProtectedRoute"
 import InputResetPassLayout from "@/layouts/auth/InputResetPassLayout"
 export default function InputResetPass() {
     return(
