f85e8342338351a9dc643688230e4d1db956c3d5
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-03-12 13:56:16 -0700

    Added Table Cards

--- src/components/table/tableSelected/TableUserSidebar.tsx
+++ src/components/table/tableSelected/TableUserSidebar.tsx
@@ -1,9 +1,16 @@
 import { Table } from "@/types/Table";
+import { IconCrown } from "@tabler/icons-react";
+import UserCard from "./userCard";
+import { Title } from "@mantine/core";
 
 export default function TableUserSidebar(props: {table: Table}) {
   return (
-    <>
-      {/* TODO: Add profile list for users in the table */}
-    </>
+    <div className="rounded-3xl bg-white shadow-red-100 shadow-xl p-5 space-y-5">
+      <Title className="text-center text-5xl font-black" variant="gradient" gradient={{from: "red.7", to: "red.4"}}>Users</Title>
+      <div className="space-y-5">
+        <UserCard name={props.table.leader} email={props.table.leader} icon={ <IconCrown className="h-5 w-5 fill-amber-500" /> }/>
+        {props.table.users.filter((user) => user != props.table.leader).map((user) => <><UserCard name={user} email={user} /></>) }
+      </div>
+    </div>
   );
 }
--- src/components/table/tableSelected/userCard.tsx
+++ src/components/table/tableSelected/userCard.tsx
@@ -1,53 +1,64 @@
 import {
-  UnstyledButton,
-  UnstyledButtonProps,
   Group,
   Avatar,
   Text,
-  createStyles,
+  UnstyledButton,
+  Menu
 } from '@mantine/core';
-import { IconChevronRight } from '@tabler/icons-react';
 
-const useStyles = createStyles((theme) => ({
-  user: {
-    display: 'block',
-    width: '100%',
-    padding: theme.spacing.md,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+import { forwardRef } from "react"
 
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-    },
-  },
-}));
 
-interface UserButtonProps extends UnstyledButtonProps {
-  image: string;
+interface UserCardProps extends React.ComponentPropsWithoutRef<'button'> {
+  image?: string;
   name: string;
   email: string;
   icon?: React.ReactNode;
 }
 
-export function UserButton({ image, name, email, icon, ...others }: UserButtonProps) {
-  const { classes } = useStyles();
-
-  return (
-    <UnstyledButton className={classes.user} {...others}>
-      <Group>
+// eslint-disable-next-line react/display-name
+const UserButton = forwardRef<HTMLButtonElement, UserCardProps>(
+  ({ image, name, email, icon, ...others }: UserCardProps, ref) => (
+    <UnstyledButton ref={ref}>
+      <Group className='bg-rose-100 p-5 shadow-rose-200 rounded-3xl shadow-lg transition-all ease-in-out hover:shadow-xl hover:shadow-red-300 delay-100 duration-500'>
         <Avatar src={image} radius="xl" />
-
         <div style={{ flex: 1 }}>
           <Text size="sm" weight={500}>
             {name}
           </Text>
-
           <Text color="dimmed" size="xs">
             {email}
           </Text>
         </div>
-
-        {icon || <IconChevronRight size="0.9rem" stroke={1.5} />}
+        { icon }
       </Group>
     </UnstyledButton>
+  )
+);
+
+export default function UserCard({ image, name, email, icon }: UserCardProps) {
+
+  return (
+    <Group position="center">
+      <Menu>
+        <Menu.Target>
+          <UserButton
+            image={image}
+            name={name}
+            email={email}
+            icon={icon}
+          />
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            component="a"
+            href="https://mantine.dev"
+            target="_blank"
+          >
+            External link
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </Group>
   );
 }
