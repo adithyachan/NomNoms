9c7539adad11429eedfedbcb4e3d20a149a261b0
albearx <albert.xu.13103@icloud.com>
2023-03-29 18:38:03 -0400

    Added voting page unique to tables

---
+++ src/components/table/restaurantCards/Voting.tsx
@@ -0,0 +1,41 @@
+import CardStack from "@/components/table/restaurantCards/CardStack";
+import { useRestaurantListEndpoint } from "@/lib/utils/yelpAPI";
+import { Loader } from "@mantine/core";
+import { useState } from "react";
+import FavoritePicker from "@/components/table/restaurantCards/FavoritePicker";
+import { useRouter } from "next/router";
+
+export default function Voting({zip, prefs}: any) {
+  const [state, setState] = useState('stack'); // can be stack, favorite, done
+  const [votes, setVotes] = useState();
+  const { data: listData, error: listError, isLoading: listIsLoading } = useRestaurantListEndpoint(10, zip, 3200, prefs);
+  const router = useRouter()
+  if (state === 'stack') {
+    if (listError) {
+      return <>Error loading restaurants</>;
+    }
+    if (listIsLoading) {
+      return <Loader color="FF5858" />;
+    } else {
+      const ids = listData.businesses.map((business: any) => business.id);
+      return <CardStack ids={ids} setState={setState} setUserVotes={setVotes}/>;
+    }
+  } else if (state === 'favorite') {
+
+    let ids: any[] = []
+    if (votes) {
+      ids = Object.keys(votes)
+    } else {
+      ids = []
+    }
+    if (ids.length <= 1) {
+      setState('complete')
+    }
+
+    return <FavoritePicker ids={ids} votes={votes} setVotes={setVotes} setState={setState} listData={listData.businesses}/>
+  } 
+    // votes variable should be sent to database somewhere to be aggregated along with others' votes
+    // user should be redirected to waiting page
+  router.push('/voting/results')
+  return <></>
+}
---
+++ src/pages/tables/[tableid]/voting.tsx
@@ -0,0 +1,25 @@
+import { Table } from "@/types/Table"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+import { ReadTable } from "@/lib/firebase/table/TableOperations"
+import Voting from "@/components/table/restaurantCards/Voting"
+
+export default function RestaurantVoting() {
+  const router = useRouter()
+  const { tableid } = router.query
+  const[table, setTable] = useState<Table>()
+
+  useEffect(() => {
+      if (tableid !== undefined) {
+        const unsub = ReadTable(tableid as string, setTable)
+        return unsub
+      }
+  }, [tableid])
+  
+  
+  return (
+    <>
+      <Voting zip={table?.prefs.zip ?? 10019} prefs={table?.prefs.cuisine ?? 'indian'} />
+    </>
+  )
+}
