a17d5cf129771c923aae4803406329b28933a218
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2023-03-30 19:00:56 -0400

    all sorting functionality

--- src/components/table/restaurantCards/LexSort.tsx
+++ src/components/table/restaurantCards/LexSort.tsx
@@ -1,34 +1,14 @@
-import { useRestaurantBusinessEndpoint } from "@/lib/utils/yelpAPI";
-import CardStack from "./CardStack";
-
-export default function LexSort(props : {ids : any , ascending : boolean}) {
-    let list : (string|number)[][] = []
-    for (var i = 0; i < props.ids.length; i++) {
-        const {data : businessData, error : businessError , isLoading: isLoadingBusiness} = useRestaurantBusinessEndpoint(props.ids[i])
-        var name = businessData?.name
-        list[i] = [props.ids[i], name]
-    }
-    var temp = list[0]
+export default function SortByLex(props: { hashm: Map<any, any>, ascending: boolean }): string[] {
     if (props.ascending) {
-        for(var i = 0; i < props.ids.length-1; i++) {
-            for (var j = 0; j < props.ids.length-i-1; j++) {
-                if (list[j][1] > list[j+1][1]) {
-                    temp = list[j];
-                    list[j] = list[j+1];
-                    list[j+1] = temp;
-                }
-            }
-        }
+      const entries = Array.from(props.hashm.entries());
+      entries.sort((a, b) => a[1].toString().toLowerCase().localeCompare(b[1].toString().toLowerCase()));
+      const keys = entries.map((entry) => entry[0].toString());
+      return keys;
     } else {
-        for(var i = 0; i < props.ids.length-1; i++) {
-            for (var j = 0; j < props.ids.length-i-1; j++) {
-                if (list[j][1] < list[j+1][1]) {
-                    temp = list[j];
-                    list[j] = list[j+1];
-                    list[j+1] = temp;
-                }
-            }
-        }
+      const entries = Array.from(props.hashm.entries());
+      entries.sort((a, b) => b[1].toString().toLowerCase().localeCompare(a[1].toString().toLowerCase()));
+      const keys = entries.map((entry) => entry[0].toString());
+      return keys;
     }
-    return list.map((entry) => entry[0]);
-}
+  }
+  
