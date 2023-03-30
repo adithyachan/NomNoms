9b3c7dc8d12d9920111f05ecf7b61d26fdf84f0d
Jay Mehta <jaysmehta@gmail.com>
2023-03-30 01:15:40 -0400

    Worked Jay and Dhruv on sorting display

--- src/components/table/restaurantCards/CardStack.tsx
+++ src/components/table/restaurantCards/CardStack.tsx
@@ -1,23 +1,61 @@
 import ShowCard from "./Card";
-import React, { useEffect, useState } from 'react'
+import React, {  ReactNode, useEffect, useState } from 'react'
 import { IconArrowBack, IconCheck, IconThumbDown, IconThumbUp, IconX, IconChevronRight} from '@tabler/icons-react'
 
 import { Card, Text, Button, Tooltip, Progress, NavLink } from '@mantine/core';
+import { JsxElement } from "typescript";
+type ComponentProps = {
+  id: string;
+};
 
+type ComponentMap = {
+  [key: string]: React.FC<ComponentProps>;
+};
+const componentMap: ComponentMap = {
+  myComponent: ShowCard,
+};
+const DynamicComponent = componentMap["myComponent"];
+export default function CardStack({listData,ids, setState, setUserVotes, user} : any) {
+  //console.log(listData.businesses)
+  var prices = new Map()
+  var names = []
+  var reviewNos = []
+  var ratings = []
+  var neededBusiness :any
+  var price = ""
+  const cards3  = new Map
+//  {[key:string] : typeof DynamicComponent}= {}
+  for(var i =0; i < ids.length;i++) {
+    //console.log(listData[0])
+    neededBusiness = listData.businesses[i]
+    price = neededBusiness.price 
+    prices.set(ids[i], price)
+   cards3.set(ids[i], <DynamicComponent id = {ids[i]} />)
 
-export default function CardStack({ids, setState, setUserVotes, user} : any) {
-  const cards = ids.map((id: string) => <ShowCard key={id} id={id}/>)
+  }
+
+  //onst cards = [cards3.get(ids[0]), cards3.get(ids[1]), cards3.get(ids[2]) ]
+ //lists start
+  //const cards = ids.map((id: string) => <ShowCard key={id} id={id}/>)
+  const [ids1, setIds] = useState(ids)
+  const [cards, setCards] = useState([cards3.get(ids1[0]), cards3.get(ids1[1]), cards3.get(ids1[2]) ])
   const [index, setIndex] = useState(0)
   const [card, setCard] = useState(cards[index])
   const [canFinish, setCanFinish] = useState(false)
   const [canSkip, setCanSkip] = useState(true)
+  const [flag, setFlag] = useState(false)
+  //setCards( [cards3.get(ids[1]), cards3.get(ids[0]), cards3.get(ids[2]) ])
+
   // not to be confused with setUserVotes, which is a setter function passed into this component
   // in order to set this user's votes. 
   // setVotes is meant to update the user's votes as they are going through the card stack,
   // setUserVotes will update the user's votes in the Table once the user hits Done.
 
-  const [votes, setVotes] = useState(ids.reduce((acc: any, cur: any) => ({...acc, [cur]: 0}), {}))
-
+  const [votes, setVotes] = useState(ids1.reduce((acc: any, cur: any) => ({...acc, [cur]: 0}), {}))
+  console.log(cards.length)
+  
+  //setCards([cards3.get(ids[0]), cards3.get(ids[1]), cards3.get(ids[2])])
+  
   cards.push(
     <div style={{height: '450px', width: '450px'}}>
       <Card withBorder radius='md' style={{height: '100%'}}>
@@ -32,12 +70,34 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
     </div>
     
   )
+     useEffect(() => {
+      console.log("useeffect")
+      console.log(flag)
+      if (flag) {
+        console.log("cards change")
+      // var cards4 = []
+      //  for (var i = 0; i < ids.length; i++) {
+      //     cards4[i] = cards3.get(ids[i])
+      //  }
+       setCards([cards3.get(ids1[0]), cards3.get(ids1[1]), cards3.get(ids1[2]) ])
+       setIndex(0)
+       setCard(cards[0])
+       setCanSkip(false)
+       setCanFinish(false)
+       setVotes(ids1.reduce((acc: any, cur: any) => ({...acc, [cur]: 0}), {})) 
+      setFlag ( false)
+     } else {
+      setCard(cards[index])
+      console.log("index change")
+      if (index == cards.length - 1)
+        setCanFinish(true); 
+     }},[ids1,index])
 
-  useEffect(() => {
-    setCard(cards[index])
-    if (index == cards.length - 1)
-      setCanFinish(true);
-  }, [index])
+  // useEffect(() => {
+  //   setCard(cards[index])
+  //   if (index == cards.length - 1)
+  //     setCanFinish(true);
+  // }, [index])
 
   
   function handleYesClick() {
@@ -46,7 +106,7 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
       setCanSkip(false);
       setVotes((votes: any) => ({
         ...votes, 
-        [ids[index]]: 1,
+        [ids1[index]]: 1,
       }));
     }
   }
@@ -56,7 +116,7 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
       setCanSkip(false);
       setVotes((votes: any) => ({
         ...votes, 
-        [ids[index]]: 0,
+        [ids1[index]]: 0,
       }));
     }
   }
@@ -79,6 +139,16 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
     // setUserVotes(votes)
     // TODO: redirect to waiting for other nomsters to finish their votes page
   }
+
+  function handleSort() {
+    console.log(index)
+    setFlag (true)
+    const r = ids1[2]
+  const g = ids1[1]
+  const b = ids1[0]
+  setIds( [r,g,b])
+  console.log(index)
+  }
   
   function FinishButton() {
     if (! canFinish) {
@@ -100,6 +170,7 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
     }
   }
   function YesButton() {
+    
     if (index == cards.length - 1) {
       return (
         <Button size="lg" disabled variant='light' radius='xl' color="green">
@@ -129,6 +200,13 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
       )
     }
   }
+  function TestButton() {
+    //flag = true
+    
+    return (<Button size="lg" onClick={handleSort} variant='light' radius='xl' color="yellow">
+          <IconArrowBack color='black' size={20} stroke={1.5} />
+        </Button>)
+  }
   function BackButton() {
     if (index == 0) {
       return (
@@ -157,7 +235,7 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
     }
   }
 
-  if (ids == undefined || ids.length == 0) return (<Text size="md" color="dimmed">Card stack is empty!</Text> )
+  if (ids1 == undefined || ids1.length == 0) return (<Text size="md" color="dimmed">Card stack is empty!</Text> )
 
   else{
     return (
@@ -174,7 +252,7 @@ export default function CardStack({ids, setState, setUserVotes, user} : any) {
               <YesButton />
               <NoButton />
               <FinishButton />
-              
+              <TestButton />
             </div>
             <Skip />
           </div>
