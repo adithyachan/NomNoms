98ee328dd7aa1e331a6cece3f86d59dcc577e6c6
Jay Mehta <jaysmehta@gmail.com>
2023-04-20 03:08:27 -0400

    adding navbar

--- src/pages/auth/changePass.tsx
+++ src/pages/auth/changePass.tsx
@@ -1,9 +1,21 @@
+import NavBar from "@/components/NavBar"
 import ChangePasswordLayout from "@/layouts/auth/ChangePasswordLayout"
+import { Container, Grid } from "@mantine/core"
+import { NotificationsProvider } from "@mantine/notifications";
 
 export default function ChangePassword() {
 return(
     <>
-      < ChangePasswordLayout/>
+    <NavBar >
+    </NavBar>
+<Container fluid className="bg-gradient-to-b from-rose-100 to-white">
+
+<Grid>
+    <Grid.Col span={12} md={4.2}></Grid.Col>
+      < ChangePasswordLayout />
+    </Grid>
+    </Container>
+   
     </>
     );
 }
