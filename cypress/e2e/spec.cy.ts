c51a590586b900c7d80d38b268ec658b7481c26b
rjagana <rjagana@purdue.edu>
2023-04-20 23:42:27 -0400

    few test cases

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -26,3 +26,39 @@ describe('Login Test', () => {
     
   })
 })
+
+//This test case will only function on the first time purposefully because
+//after the first time, the email would have already existed and won't be able
+//to create an account
+describe('Create an Account test', () => {
+  it('Create an account', () => {
+    //visit the app's homepage
+    cy.visit('http://localhost:3000')
+
+    //attempt to create an account
+   cy.contains('Sign Up').click()
+   cy.get('input[placeholder="nomnoms@gmail.com"]').type('ruchi_jag@gmail.com')
+   cy.get('input[placeholder="SpicyBurrito"]').type('DeliciousTostada')
+   cy.get('input[placeholder="Your password"]').type('thisismypassword')
+   cy.get('input[placeholder="Confirm your password"]').type('thisismypassword')
+   cy.get('input[type="checkbox"]').check()
+   cy.contains('Register').click()
+   cy.wait(6000)
+   cy.contains('Awaiting Email Verification').should('exist')
+  })
+})
+
+describe ('Sign in with existing account test', () => {
+  it('Sign in with google', () => {
+    //visit the app's homepage
+    cy.visit('http://localhost:3000')
+
+    //attempt to create an account
+    cy.contains('Log In').click()
+    cy.get('input[placeholder="nomnoms@gmail.com"]').type('rpjagana@gmail.com')
+    cy.get('input[placeholder="Your password"]').type('ruchitha')
+    cy.contains('Login').click()
+
+  })
+})
+  
